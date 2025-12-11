0145f61c280144d7ffd12325fb01a1c8298eb034
sai kiran pathivada <saip73191@gmai.com>
2025-12-12 05:28:53 +0530

    v1 is completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="troll-face.png" 
+                className="header--image"
+                alt="Troll Face"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
